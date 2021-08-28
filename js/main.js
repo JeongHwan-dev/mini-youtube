5ac990f154f04d96da71c540bba84c57ae27d6ab
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-08-28 20:06:21 +0900

    Feat : 구독 버튼 활성화 및 알람 버튼 생성, 채널 설명 문구 구역 추가

--- js/main.js
+++ js/main.js
@@ -4,7 +4,25 @@
 const moreBtn = document.querySelector('.info .metadata .moreBtn');
 const title = document.querySelector('.info .metadata .title');
 
+// 영화 제목 더 보기 버튼 클릭 시
 moreBtn.addEventListener('click', () => {
   moreBtn.classList.toggle('clicked');
   title.classList.toggle('clamp');
+});
+
+const subscribeBtn = document.getElementById('subscribe');
+const alarmBtn = document.getElementById('alarm');
+
+// 구독 버튼 클릭 시
+subscribeBtn.addEventListener('click', () => {
+  subscribeBtn.classList.toggle('clicked');
+  alarmBtn.classList.toggle('hidden');
+
+  const isClicked = subscribeBtn.classList.contains('clicked');
+
+  if (isClicked) {
+    subscribeBtn.innerText = '구독중';
+  } else {
+    subscribeBtn.innerText = '구독';
+  }
 });
